7ef0e5ddf702a3ede5ee00fcb856e78870dc5cfd
KhinThandarTun <106916866+KhinThandarTun@users.noreply.github.com>
2023-11-21 10:36:04 +0630

    Update

--- products.js
+++ products.js
@@ -2,26 +2,26 @@
 let products = [
   {
     id: 1,
-    name: "Macbook 1",
+    name: "Item 1",
     price: 100,
-    src: "./img/lp-2.jpg",
+    src: "./img/dress-1.jpg",
   },
   {
     id: 2,
-    name: "Macbook 2",
+    name: "Item 2",
     price: 200,
-    src: "./img/lp-3.jpg",
+    src: "./img/dress-2.jpg",
   },
   {
     id: 3,
-    name: "Macbook 3",
+    name: "Item 3",
     price: 300,
-    src: "./img/lp-4.jpg",
+    src: "./img/dress-3.jpg",
   },
   {
     id: 4,
-    name: "Macbook 4",
-    price: 400,
-    src: "./img/lp-5.jpg",
-  },
+    name: "Item 4",
+    price: 300,
+    src: "./img/dress-4.jpg",
+  }
 ];
